6d258ae7aaa9aec707661abf8299da2458c633ec
Thomas VIAL <github@ifusio.com>
2016-06-09 08:28:48 +0200

    Added allow-query from any.

--- start-bind.sh
+++ start-bind.sh
@@ -7,7 +7,7 @@ chown root:root /etc/bind/rndc.key
 chmod 755 /etc/bind/rndc.key
 
 echo "Configuring 'named.conf.options'"
-sed -i '/options {/a\\tallow-recursion { localnets; localhost; };\n\tallow-query-cache { localnets; localhost; };\n\tforwarders { 8.8.8.8; 8.8.4.4; };\n' /etc/bind/named.conf.options
+sed -i '/options {/a\\tallow-recursion { localnets; localhost; };\n\tallow-query-cache { any; };\n\tforwarders { 8.8.8.8; 8.8.4.4; };\n\tallow-query { any; };\n' /etc/bind/named.conf.options
 
 echo "Generating 'named.conf.local'"
 rm /etc/bind/named.conf.local
